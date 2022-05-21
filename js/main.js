7d968feb7d572c8a024550713212f8859ec76b58
Fredy Lucho <fredy.learning@gmail.com>
2022-05-21 02:30:47 -0500

    Version Mobile Efectivo a Cambios

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function scrollActive(){
     const scrollY = window.pageYOffset
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 100;
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.c-nav-menu a[href*=' + sectionId + ']').classList.add('c-active-link')
